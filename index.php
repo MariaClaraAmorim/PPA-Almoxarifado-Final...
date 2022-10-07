b1ce944de2ed69f1ae0319eec7d7dc96c5eb2948
maria.amorim <maria.amorim@dantaseletro.com.br>
2022-10-07 18:29:44 -0300

    feat: adiciona rota de login

--- index.php
+++ index.php
@@ -17,6 +17,12 @@ include __DIR__ . '/routes/Admin.php';
 //INCLUI AS ROTAS DAS PÁGINAS DE ADMIN
 include __DIR__.'/routes/Usuario.php';
 
+//INCLUI A ROTA DA PÁGINA DE LOGIN
+include __DIR__.'/routes/login.php';
+
+//INCLUI A ROTA DA PÁGINA DE CADASTRO
+include __DIR__.'/routes/cadastro.php';
+
 // IMPRIME O RESPONSE DA ROTA
 $obRouter->run()
     ->sendResponse();
