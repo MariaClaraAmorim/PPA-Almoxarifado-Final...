31b396243c47d30ce2bc9d46ded56cb5b8669d58
maria.amorim <maria.amorim@dantaseletro.com.br>
2022-10-10 18:22:42 -0300

    feat: adiciona função de login

--- App/Controller/Pages/Login.php
+++ App/Controller/Pages/Login.php
@@ -2,11 +2,16 @@
 
 namespace App\Controller\Pages;
 
+use App\Model\Entity\Usuario;
+
+use App\Session\Usuario\Login as UsuarioLogin;
+
 use \App\Utils\View;
 
-class Login extends Page{
+class Login extends Page
+{
   /**
-   * Método responsável por retornar a renderização da página de login
+   * Método responsável por retornar a rederização da página de login
    * @param Request $request
    * @return string
    */
@@ -17,5 +22,82 @@ class Login extends Page{
       'usuario' => '',
       'senha'   => ''
     ]);
+  }
+
+  /**
+   * Método responsável por definir o login do usuário
+   * @param Request $request
+   */
+  public static function setLogin($request)
+  {
+    //POST VARS
+    $postVars = $request->getPostVars();
+    $usuario    = $postVars['usuario'];
+
+    //BUSCAR USUÁRIO PELO LOGIN
+    $obUser = Usuario::getUserByUser($usuario);
+
+    //CRIA A SESSÃO DE LOGIN
+    UsuarioLogin::login($obUser);
+
+    return true;
+  }
+
+  /**
+   * Método responsável por validar a criptográfia da senha
+   */
+  public static function validatePassword($request)
+  {
+    $postVars = $request->getPostVars();
+
+    if ($postVars['senha_admin']) {
+      $senha = $postVars['senha_admin'];
+    } else if ($postVars['senha_atual']) {
+      $senha = $postVars['senha_atual'];
+    }
+
+    //BUSCAR AS INFORMAÇÕES DO USUÁRIO PELO USER
+    $obUser = Usuario::getUserByUser($request->user->LOGIN);
+
+    //VERIFICA A SENHA DO USUÁRIO
+    if (password_verify($senha, $obUser->SENHA)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+
+  /**
+   * Método responsável por validar a cripotográfia da senha
+   */
+  public static function validatePasswordLogin($request)
+  {
+    $postVars = $request->getPostVars();
+    $usuario = $postVars['usuario'];
+    $senha = $postVars['senha'];
+
+    //BUSCAR AS INFORMAÇÕES DO USUÁRIO PELO USER
+    $obUser = Usuario::getUserByUser($usuario);
+
+    //VERIFICA A SENHA DO USUÁRIO
+    if (password_verify($senha, $obUser->SENHA)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * Método responsável por deslogar o usuário
+   * @param Request $request
+   */
+  public static function setLogout($request)
+  {
+    //DESTROI A SESSÃO DE LOGIN
+    UsuarioLogin::logout();
+
+    //REDIRECIONA O USUÁRIO PARA A TELA DE LOGIN
+    $request->getRouter()->redirect('/');
   }
 }
